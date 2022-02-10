80687e245aa054ef76eddc7007ff443ba6ddb34d
EstherVlp <35601411+EstherVlp@users.noreply.github.com>
2022-02-10 12:16:53 +0100

    Feat : Page Wall fini

--- wall.php
+++ wall.php
@@ -51,8 +51,7 @@
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : XXX
-                        (n° <?php echo $userId ?>)
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
                     </p>
                 </section>
             </aside>
@@ -85,24 +84,20 @@
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    /* echo "<pre>" . print_r($post, 1) . "</pre>"; */
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <!-- TODO : mettre en forme la date-->
+                            <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo $post['author_name'] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post['content'] ?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number'] ?></small>
+                            <a href="">#<?php echo $post['taglist'] ?></a>
                         </footer>
                     </article>
                 <?php } ?>
