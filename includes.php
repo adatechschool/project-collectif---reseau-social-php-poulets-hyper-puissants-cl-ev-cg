adfd791ee5d215a3ded23b3abaa300dd540c0c9d
Astating <cguilb3@gmail.com>
2022-02-10 10:34:12 +0100

    FIX: removing trailing echos + placeholders in includes.php and news.php

--- includes.php
+++ includes.php
@@ -1,5 +1,4 @@
 <?php
 
 $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-echo "<p>" . "turtle" . "</p>";
 ?>
--- news.php
+++ news.php
@@ -36,7 +36,7 @@
             <main>
                 <!-- L'article qui suit est un exemple pour la présentation et 
                   @todo: doit etre retiré -->
-                <article>
+               <!--  <article>
                     <h3>
                         <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
                     </h3>
@@ -53,7 +53,7 @@
                         <a href="">#lorem</a>,
                         <a href="">#piscitur</a>,
                     </footer>
-                </article>               
+                </article>    -->            
 
                 <?php
                 include "includes.php";
@@ -111,8 +111,8 @@
                 {
                     //la ligne ci-dessous doit etre supprimée mais regardez ce 
                     //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
-
+                   /*  echo "<pre>" . print_r($post, 1) . "</pre>";
+ */
                     // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
                     // ci-dessous par les bonnes valeurs cachées dans la variable $post 
                     // on vous met le pied à l'étrier avec created
