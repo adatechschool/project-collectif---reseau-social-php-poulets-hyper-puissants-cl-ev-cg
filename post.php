018e571ebc29b2c6b542085c09fadad606c98006
celinelouaintier <celine.louaintier@gmail.com>
2022-02-16 16:46:50 +0100

    Fix : remttre balise article dans post.php

--- post.php
+++ post.php
@@ -47,6 +47,7 @@ if ($enCoursDeLike){
                             <input type='hidden' name='post_id' value="{$post['post_id']}">
                             <input type='submit' value='more muscle'>
                         </form>
+                    </article>
                     END;
                     echo $post_article;
                     ?>
